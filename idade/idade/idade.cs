3e4b402ad122ad0e9118b83033b491143e8ec99e
Miguel-Molina0 <167210792+Miguel-Molina0@users.noreply.github.com>
2025-02-18 17:12:25 -0300

    Add files via upload

---
+++ idade/idade/idade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string nome;
+        double idade;
+        
+        Console.WriteLine("digite seu nome");
+        nome = Console.ReadLine();
+
+        Console.WriteLine("digite sua idade");
+        idade = Convert.ToDouble(Console.ReadLine());
+
+        if(idade >= 18)
+        {
+            Console.WriteLine (nome,  "é maior de idade e tem" ,idade, " anos");
+        }
+    }
+}
